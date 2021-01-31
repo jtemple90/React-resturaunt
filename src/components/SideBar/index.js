20339f8f98e78205863ea2d0a9c121066d0a8fdd
John Temple <johntemple@Alenas-iMac.local>
2021-01-30 21:49:37 -0700

    animated sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,33 +1,32 @@
 import React from 'react'
-// import { IconBase } from 'react-icons/lib'
-import {  
-  SidebarContainer, 
-  Icon, 
+import { IconBase } from 'react-icons/lib'
+import {
+  SidebarContainer,
+  Icon,
   CloseIcon,
-  SidebarMenu, 
+  SidebarMenu,
   SidebarLink,
-  SidebarRoute, 
-  SidebarBtnWrap 
+  SidebarRoute,
+  SidebarBtnWrap
 } from './SideBarElements'
 
 const Sidebar = ({isOpen, toggle}) => {
   return (
     <SidebarContainer isOpen={isOpen} onClick={toggle}>
-      <Icon onClick={toggle}>
-        <CloseIcon />
-      </Icon>
+    <Icon onClick={toggle}>
+      <CloseIcon/>
+    </Icon>
       <SidebarMenu>
-        <SidebarLink to="/">Drinks</SidebarLink>
-        <SidebarLink to="/">Food</SidebarLink>
-
-        <SidebarLink to="/">About Us</SidebarLink>
-        <SidebarLink to="/">Contact Us</SidebarLink>
+        <SidebarLink to='/'>Drinks</SidebarLink>
+        <SidebarLink to='/'>Food</SidebarLink>
+        <SidebarLink to='/'>About Us</SidebarLink>
+        <SidebarLink to='/'>Contact Us</SidebarLink>
       </SidebarMenu>
       <SidebarBtnWrap>
-        <SidebarRoute to="/">Order Now</SidebarRoute>
+        <SidebarRoute to='/'>Order Now</SidebarRoute>
       </SidebarBtnWrap>
     </SidebarContainer>
-  );
+  )
 }
 
 export default Sidebar
